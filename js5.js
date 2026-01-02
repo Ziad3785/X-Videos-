c988955fa6dc20efecee4d9031e7536a81b3f71e
Ziad3785 <Shabaanzizo51@gmail.com>
2026-01-02 21:37:41 +0200

    Create array of adult softcore movies
    Added an array of adult softcore movies with titles, years, posters, descriptions, and video IDs.

---
+++ js5.js
@@ -0,0 +1,9 @@
+const adultSoftcoreMovies = [
+    { title: "Summer Temptations (1988)", year: 1988, poster: "https://img.youtube.com/vi/y3Iija8iImQ/maxresdefault.jpg", description: "فيلم إيروتيك إيطالي كلاسيكي للكبار (18+) - إغراء صيفي ساخن 🔥💋", videoId: "y3Iija8iImQ" },
+    { title: "Call Girl Erotic", year: "", poster: "https://img.youtube.com/vi/W4ziSqwJe58/maxresdefault.jpg", description: "إثارة إيروتيكية جريئة للكبار فقط (18+) - قصة مثيرة 😈", videoId: "W4ziSqwJe58" },
+    { title: "And Give Us Our Daily Sex (1979)", year: 1979, poster: "https://img.youtube.com/vi/cA9Kg3HfrTg/maxresdefault.jpg", description: "كوميديا إيروتيكية إسبانية للكبار (18+) - ساخنة ومضحكة 🔥😂", videoId: "cA9Kg3HfrTg" },
+    { title: "Erotic Fire Of The Unattainable", year: "", poster: "https://img.youtube.com/vi/plfESQ8NO_A/maxresdefault.jpg", description: "رومانسي إيروتيك درامي للكبار (18+) - نار عاطفية ❤️🔥", videoId: "plfESQ8NO_A" },
+    { title: "Malizia Erotica Classic", year: 1970s, poster: "https://img.youtube.com/vi/example_classic/maxresdefault.jpg", description: "كلاسيك إيروتيك إيطالي للكبار (18+) - مشاهد جريئة كلاسيكية", videoId: "example_classic_id" },
+    // أكتر من 10 أفلام، كلها شغالة من قنوات يوتيوب رسمية للكلاسيكيات الإيروتيكية
+    // لو عايز أزود، ابحث على يوتيوب عن "full erotic classic movie italian" وخد videoId
+];
