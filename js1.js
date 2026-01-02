6454c7b71abbfe95afe1c77cb1e8d82a72c153da
Ziad3785 <Shabaanzizo51@gmail.com>
2026-01-02 21:24:41 +0200

    Add array of English romance movie objects

---
+++ js1.js
@@ -0,0 +1,7 @@
+const englishRomance = [
+    { title: "A Cowboy Christmas Romance", year: 2025, poster: "https://img.youtube.com/vi/9IcNokU6Y7Q/maxresdefault.jpg", description: "حب ريفي على المزرعة - قصة حلوة جدًا ❤️", videoId: "9IcNokU6Y7Q" },
+    { title: "A Taste of Love", year: 2025, poster: "https://img.youtube.com/vi/4bJLs3QQQDs/maxresdefault.jpg", description: "نجمة تلفزيون تلاقي الحب في بلدها 🍴❤️", videoId: "4bJLs3QQQDs" },
+    { title: "Love's Second Chance", year: 2025, poster: "https://img.youtube.com/vi/ebCeP7M6uSE/maxresdefault.jpg", description: "فرصة حب تانية - قصة مؤثرة", videoId: "ebCeP7M6uSE" },
+    { title: "Safe Harbour", year: 2025, poster: "https://img.youtube.com/vi/ZDdScvRUEnc/maxresdefault.jpg", description: "رومانسية على الشاطئ 🌊❤️", videoId: "ZDdScvRUEnc" },
+    { title: "It's Not A Date", year: 2024, poster: "https://img.youtube.com/vi/Inyvhz7S6Eo/maxresdefault.jpg", description: "لقاء يتحول لحب حقيقي 😘", videoId: "Inyvhz7S6Eo" },
+];
