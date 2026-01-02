759a1cea6fd8dce2c6a7e0b05363b316d28a1388
Ziad3785 <Shabaanzizo51@gmail.com>
2026-01-02 21:39:17 +0200

    Add array of adult movie objects

---
+++ js3.js
@@ -0,0 +1,7 @@
+const adultMovies = [
+    { title: "Summer Temptations", year: 1988, poster: "https://img.youtube.com/vi/y3Iija8iImQ/maxresdefault.jpg", description: "فيلم إيروتيك إيطالي كلاسيكي للكبار فقط (18+) 🔥 - مشاهد ساخنة ورومانسية", videoId: "y3Iija8iImQ" },
+    { title: "Call Girl", year: "", poster: "https://img.youtube.com/vi/W4ziSqwJe58/maxresdefault.jpg", description: "إثارة إيروتيكية للكبار (18+) - قصة مثيرة وجريئة 💋", videoId: "W4ziSqwJe58" },
+    { title: "Wild Target (Erotic Comedy)", year: 2010, poster: "https://img.youtube.com/vi/sbmd6aMyS84/maxresdefault.jpg", description: "كوميديا إيروتيكية للكبار مع بيل ناي (18+) 😂🔥", videoId: "sbmd6aMyS84" },
+    { title: "Law of Attraction", year: 2024, poster: "https://img.youtube.com/vi/3fceYbqtoZ0/maxresdefault.jpg", description: "رومانسي إيروتيك للكبار - قصة حب ساخنة (18+)", videoId: "3fceYbqtoZ0" },
+    // أفلام إيروتيك خفيف متاحة على قنوات رسمية - لو اتشال فيديو، ابحث عن بديل في القنوات دي
+];
