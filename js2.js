d3092699e8675e060de1e6e792d609e9b440c2b0
Ziad3785 <Shabaanzizo51@gmail.com>
2026-01-02 21:26:37 +0200

    Add array of Egyptian romance movies

---
+++ js2.js
@@ -0,0 +1,5 @@
+const egyptianRomance = [
+    { title: "خلوة خاصة", year: 2024, poster: "https://img.youtube.com/vi/5IfZ5vIywGU/maxresdefault.jpg", description: "هاني سلامة ودرة - حب عميق ورومانسي ❤️", videoId: "5IfZ5vIywGU" },
+    { title: "بحبك", year: 2022, poster: "https://img.youtube.com/vi/example/maxresdefault.jpg", description: "تامر حسني - قصة حب مصرية حديثة", videoId: "example_id" },
+    { title: "فيلم رومانسي مصري كلاسيك", year: "", poster: "https://img.youtube.com/vi/another/maxresdefault.jpg", description: "قصة حب من زمن الفن الجميل", videoId: "another_id" },
+];
