7fed5db03ec520a94ac21500b546449cfda0182c
Ziad3785 <Shabaanzizo51@gmail.com>
2026-01-02 21:38:32 +0200

    Add list of adult erotic movies with details

---
+++ js4.js
@@ -0,0 +1,7 @@
+const adultEroticMovies = [
+    { title: "Summer Temptations", year: 1988, poster: "https://img.youtube.com/vi/y3Iija8iImQ/maxresdefault.jpg", description: "فيلم إيروتيك إيطالي كلاسيكي للكبار فقط (18+) - مشاهد ساخنة وإثارة 🔥💋", videoId: "y3Iija8iImQ" },
+    { title: "Call Girl", year: "", poster: "https://img.youtube.com/vi/W4ziSqwJe58/maxresdefault.jpg", description: "إثارة إيروتيكية جريئة للكبار (18+) - قصة مثيرة جدًا 😈", videoId: "W4ziSqwJe58" },
+    { title: "And Give Us Our Daily Sex", year: 1979, poster: "https://img.youtube.com/vi/cA9Kg3HfrTg/maxresdefault.jpg", description: "فيلم إيروتيك كلاسيكي إسباني للكبار (18+) - كوميديا ساخنة 🔥", videoId: "cA9Kg3HfrTg" },
+    { title: "Erotic Fire Of The Unattainable", year: "", poster: "https://img.youtube.com/vi/plfESQ8NO_A/maxresdefault.jpg", description: "رومانسي إيروتيك درامي للكبار (18+) - قصة حب ساخنة ❤️🔥", videoId: "plfESQ8NO_A" },
+    // كل الروابط دي شغالة ومن قنوات رسمية على يوتيوب
+];
